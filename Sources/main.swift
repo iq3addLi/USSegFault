c584d64c3fb2269e1fb0233e137b86a496f6c838
iq3 <iq3@addli.co.jp>
2016-12-08 12:07:42 +0900

    In order to narrow down the situation, I partially commented out when the code at problem discovery.

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,17 +2,17 @@ import Foundation
 import Dispatch
 
 
-guard let url = URL(string: "https://hogehoge.api.github.com/" ) // Network Error
+guard let url = URL(string: "https://hogehoge.api.github.com/" ) // Network Error : Would you like to connect to the server anyway?
     else{ fatalError() }
-    
-guard let body = try? JSONSerialization.data(withJSONObject: [ "test" : "test" ], options: JSONSerialization.WritingOptions())
-    else{ fatalError() }
+
+//guard let body = try? JSONSerialization.data(withJSONObject: [ "test" : "test" ], options: JSONSerialization.WritingOptions())
+//    else{ fatalError() }
     
 var request = URLRequest(url:url, cachePolicy:.reloadIgnoringLocalCacheData, timeoutInterval:1)
-request.httpMethod = "POST"
-request.addValue("application/json", forHTTPHeaderField:"Content-Type")
-request.httpBody = body
-    
+//request.httpMethod = "POST"
+//request.addValue("application/json", forHTTPHeaderField:"Content-Type")
+//request.httpBody = body
+
 let session = URLSession(configuration: URLSessionConfiguration.default, delegate:nil, delegateQueue: nil)
 let semaphore = DispatchSemaphore(value: 0) // Force synchronize
     
